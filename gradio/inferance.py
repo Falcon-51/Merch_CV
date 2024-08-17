11e97c51090e2252ab21df873977e4ff770bc4f9
anton2010000 <89021397725@mail.ru>
2024-08-17 17:09:29 +0300

    Интерфейс gradio v1.0

---
+++ gradio/inferance.py
@@ -0,0 +1,44 @@
+import gradio as gr
+import PIL.Image as Image
+
+from ultralytics import ASSETS, YOLO
+
+model = YOLO("yolov8n.pt")
+
+
+def predict_image(img, conf_threshold, iou_threshold):
+    """Predicts objects in an image using a YOLOv8 model with adjustable confidence and IOU thresholds."""
+    results = model.predict(
+        source=img,
+        conf=conf_threshold,
+        iou=iou_threshold,
+        show_labels=True,
+        show_conf=True,
+        imgsz=640,
+    )
+
+    for r in results:
+        im_array = r.plot()
+        im = Image.fromarray(im_array[..., ::-1])
+
+    return im
+
+
+iface = gr.Interface(
+    fn=predict_image,
+    inputs=[
+        gr.Image(type="pil", label="Загруженное изображение"),
+        gr.Slider(minimum=0, maximum=1, value=0.25, label="Confidence threshold"),
+        gr.Slider(minimum=0, maximum=1, value=0.45, label="IoU threshold"),
+    ],
+    outputs=gr.Image(type="pil", label="Результат"),
+    title="Карельская продукция",
+    description="ОПИСАНИЕ",
+    examples=[
+        [ASSETS / "bus.jpg", 0.25, 0.45],
+        [ASSETS / "zidane.jpg", 0.25, 0.45],
+    ],
+)
+
+if __name__ == "__main__":
+    iface.launch()
