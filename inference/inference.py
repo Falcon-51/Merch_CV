4bad0730a8e7bc14ec00946233dbd28d04e31a5d
Falcon-51 <ruster478@gmail.com>
2024-08-19 11:26:08 +0300

    project fix

--- inference/inference.py
+++ inference/inference.py
@@ -1,41 +1,85 @@
 import gradio as gr  
 import PIL.Image as Image 
-from ultralytics import ASSETS, YOLO, YOLOv10
 import requests
+import os
+from ultralytics import ASSETS, YOLOv10
 from PIL import Image, ImageDraw, ImageFont
 from io import BytesIO
-import os
+from typing import Optional
 
+
+# Список для выбора весов модели
 model_choices = ["weights/YOLOV10_Karelia_v2.pt","weights/YOLOV10_Karelia.pt", "weights/yolov10n.pt", "weights/yolov10s.pt", "weights/yolov10m.pt"]
 
+# Переменные для расчёта площади и положения продукта и полок
 PRODUCTS_AREA = 0
 SHELFES_AREA = 0
 CORD_SHELFS = []
 
-names = {
-  "sampo_waffle_classic": "sampo_waffle_classic",
-  "sampo_waffle_lemon": "sampo_waffle_lemon",
-  "sl_pm_2_5_1": "slavmo_milk_2.5_1" ,
-  "sl_pm_3_5_1": "slavmo_milk_3.5_1" ,
-  "sl_pm_2_5_1_5": "slavmo_milk_2.5_1.5" ,
-  "sl_pm_3_5_1_5": "slavmo_milk_3.5_1.5",
-  "sl_pbm_0_5": "slavmo_milk_melted_3.5_0.5",
-  "ol_pm_2_5_1": "olonia_milk_2.5_1",
-  "ol_pm_3_5_1": "olonia_milk_3.5_1",
-  "ol_pbm_0_5": "olonia_milk_melted_4_0.5",
-}
+
+# names = {
+#   "sampo_waffle_classic": "sampo_waffle_classic",
+#   "sampo_waffle_lemon": "sampo_waffle_lemon",
+#   "sl_pm_2_5_1": "slavmo_milk_2.5_1" ,
+#   "sl_pm_3_5_1": "slavmo_milk_3.5_1" ,
+#   "sl_pm_2_5_1_5": "slavmo_milk_2.5_1.5" ,
+#   "sl_pm_3_5_1_5": "slavmo_milk_3.5_1.5",
+#   "sl_pbm_0_5": "slavmo_milk_melted_3.5_0.5",
+#   "ol_pm_2_5_1": "olonia_milk_2.5_1",
+#   "ol_pm_3_5_1": "olonia_milk_3.5_1",
+#   "ol_pbm_0_5": "olonia_milk_melted_4_0.5",
+# }
+
 
 def calculate_area(x_min:float, y_min:float, x_max:float, y_max:float) -> float:
+    """
+    Рассчитывает площадь прямоугольника
+
+    Параметры:
+    - x_min: координата x левого верхнего угла
+    - y_min: координата y левого верхнего угла
+    - x_max: координата x правого верхнего угла
+    - y_max: координата y правого верхнего угла
+
+    Возвращает: float
+    
+    """
+
     area = (x_max - x_min) * (y_max - y_min)
+
     return area
 
 
 def get_box_center(x_min:float, y_min:float, x_max:float, y_max:float) -> tuple[float]:
+    """
+    Находит координаты центра прямоугольника
+
+    Параметры:
+    - x_min: координата x левого верхнего угла
+    - y_min: координата y левого верхнего угла
+    - x_max: координата x правого верхнего угла
+    - y_max: координата y правого верхнего угла
+
+    Возвращает: tuple[float]
+    """
+
     center_y = (y_min + y_max) / 2
     center_x = (x_min + x_max) / 2
+
     return center_x, center_y
 
-def point_in_shelves(data, point):
+
+def point_in_shelves(data:tuple[float], point:tuple[float])-> Optional[int]:
+    """
+    Проверяем, находится ли точка внутри прямоугольника
+
+    Параметры:
+    - data: Список координат полок
+    - point: координаты точки на проверку
+
+    Возвращает: tuple[float]
+    """
+    
     x, y = point
     for (x1, y1, x2, y2, number_shelf) in data:
         # Проверяем, находится ли точка внутри прямоугольника
@@ -45,19 +89,32 @@ def point_in_shelves(data, point):
 
 
 def get_shelfs(img:Image, polka_conf:float, polka_iou:float) -> BytesIO: 
+    """
+    Обращение к модели на Roboflow для детектирования полок. \n 
+    Получает координаты и в соовтествии с ними ставит BB на исходное изображение
 
-        # Задаем необходимые параметры
+    Параметры:
+    - img: изображение
+    - polka_conf: порог уверенности
+    - polka_iou: порого IOU
+    
+    Возвращает: BytesIO
+
+    """
+
+    # Задаем необходимые параметры
     URL = "https://detect.roboflow.com/shelves-ugxt3/3"
     API_KEY = "z5gSjUxoC2gzAYUByax6"
-    CONFIDENSE = polka_conf  # Укажите нужный порог уверенности
+    CONFIDENSE = polka_conf  # Указываем порог уверенности
     IOU = polka_iou
 
     global SHELFES_AREA 
     global CORD_SHELFS
+
     # Формируем параметры запроса
     PARAMS = {
         'api_key': API_KEY,
-        'confidence': CONFIDENSE,  # Добавляем параметр confidence
+        'confidence': CONFIDENSE, 
         'iou': IOU
     }
 
@@ -65,7 +122,7 @@ def get_shelfs(img:Image, polka_conf:float, polka_iou:float) -> BytesIO:
 
     # Создаем буфер для изображения
     img_byte_arr = BytesIO()
-    img.save(img_byte_arr, format='JPEG')  # Или 'PNG', в зависимости от вашего формата
+    img.save(img_byte_arr, format='JPEG')
     img_byte_arr.seek(0)  # Возвращаемся в начало потока
 
     response = requests.post(URL, params=PARAMS, files={'file': img_byte_arr})
@@ -80,12 +137,14 @@ def get_shelfs(img:Image, polka_conf:float, polka_iou:float) -> BytesIO:
         # Создаем объект для рисования
         draw = ImageDraw.Draw(img)
 
+        # Проходим по всем предсказаниям и собираем координаты y левого верхнего угла каждого BB
         for prediction in data['predictions']:
             x1 = prediction['x'] - prediction['width'] / 2
             y1 = prediction['y'] - prediction['height'] / 2
             x2 = prediction['x'] + prediction['width'] / 2
             y2 = prediction['y'] + prediction['height'] / 2
             min_cord.append(y1)
+        # Сортируем
         min_cord.sort()
 
         # Проходим по всем предсказанным объектам
@@ -97,12 +156,14 @@ def get_shelfs(img:Image, polka_conf:float, polka_iou:float) -> BytesIO:
                 x2 = prediction['x'] + prediction['width'] / 2
                 y2 = prediction['y'] + prediction['height'] / 2
                 
-                
+                # Рассчитывем номер полки и сохраняем координаты с номером полки
                 number_shelf = abs(min_cord.index(y1) - len(min_cord))
                 CORD_SHELFS.append((x1, y1, x2, y2,number_shelf))
+
                 # Рисуем рамку
                 draw.rectangle([x1, y1, x2, y2], outline="blue", width=6)
 
+                # Считаем площадь каждой полки
                 area = calculate_area(x1,y1,x2,y2)
                 SHELFES_AREA += area
 
@@ -111,13 +172,9 @@ def get_shelfs(img:Image, polka_conf:float, polka_iou:float) -> BytesIO:
                 confidence = prediction['confidence']  # Уровень уверенности
                 text = f"({confidence:.2f}, {class_name}), Level {number_shelf}, Area:{area}"  # Форматируем текст
 
-                            # Определяем размер текста
+                # Определяем размер текста
                 font_size = int(height / 35)   # Размер шрифта
                 font = ImageFont.load_default(font_size)  # Используем стандартный шрифт
-
-                # Рисуем фон для текста
-                #draw.rectangle([x1, y1 , x1+170, y1+15], fill="red")
-
 
                 # Рисуем текст
                 draw.text((x1, y1), text, fill="white", font=font)
@@ -133,11 +190,15 @@ def get_shelfs(img:Image, polka_conf:float, polka_iou:float) -> BytesIO:
 
 
 def predict_image(img:Image, conf_threshold:float, iou_threshold:float, model_choice:list[str]) -> Image:
-    """Функция для предсказания объектов на изображении с использованием модели YOLOv8.
-    Аргументы:
+    """
+    Функция для предсказания объектов на изображении с использованием модели YOLOv8.
+
+    Параметры:
     - img: изображение для обработки.
     - conf_threshold: порог уверенности для детекции объектов.
     - iou_threshold: порог Intersection Over Union для подавления ненужных перекрывающихся объектов.
+
+    Возвращает: Image
     """
     model = YOLOv10(model_choice)
     global PRODUCTS_AREA
@@ -147,11 +208,11 @@ def predict_image(img:Image, conf_threshold:float, iou_threshold:float, model_ch
     # Прогоняем изображение через модель YOLO для детекции объектов.
     results = model.predict(
         source=img,  # Передаем изображение, которое нужно обработать.
-        conf=conf_threshold,  # Устанавливаем порог уверенности для предсказания (например, объекты с уверенностью ниже этого значения игнорируются).
+        conf=conf_threshold,  # Устанавливаем порог уверенности для предсказания.
         iou=iou_threshold,  # Устанавливаем порог для подавления перекрывающихся предсказаний.
         show_labels=True,  # Указываем, что нужно показывать метки (классы объектов) на изображении.
         show_conf=True,  # Указываем, что нужно показывать уровень уверенности для каждого объекта на изображении.
-        imgsz=640,  # Устанавливаем размер изображения (можно изменять для ускорения обработки, но с потерей точности).
+        imgsz=640,  # Устанавливаем размер изображения.
     )
 
     # Цикл по результатам предсказаний.
@@ -163,11 +224,17 @@ def predict_image(img:Image, conf_threshold:float, iou_threshold:float, model_ch
         for box in r.boxes:
             cls = r.names[int(box.cls)]  # Извлекаем название класса объекта
             conf = box.conf  # Извлекаем уверенность объекта
+
             # Извлекаем координаты ограничивающего прямоугольника
             x_min, y_min, x_max, y_max = box.xyxy[0]  # Координаты: [x1, y1, x2, y2]
+
+            # Считаем площадь каждого BB продукта
             area = calculate_area(x_min, y_min, x_max, y_max)
             PRODUCTS_AREA += area
+
+            # Находим центр BB
             center_box = get_box_center(x_min, y_min, x_max, y_max)
+            #Проверяем, находится ли продукт на полке
             shelf = point_in_shelves(CORD_SHELFS, center_box)
 
             detected_objects.append(f"Area: {area}, cls: {cls} - {conf.item()*100:.2f}%, shelf: {shelf}")
@@ -178,6 +245,13 @@ def predict_image(img:Image, conf_threshold:float, iou_threshold:float, model_ch
 
 
 def infer() -> None:
+    """
+    Запускает веб-приложение
+
+    Параметры: None
+
+    Возвращает: None
+    """
 
     # Определяем веб-интерфейс Gradio для взаимодействия с моделью YOLO.
     iface = gr.Interface(
